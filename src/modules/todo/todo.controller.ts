7aceeecc5f7583f0843aa24ec2ec28af118e3362
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-20 11:18:21 +0300

    added todo controller

--- src/modules/todo/todo.controller.ts
+++ src/modules/todo/todo.controller.ts
@@ -1,4 +1,40 @@
-import { Controller } from '@nestjs/common';
+import {
+  Controller,
+  Body,
+  Get,
+  Post,
+  Put,
+  Param,
+  Delete,
+} from '@nestjs/common';
+
+import { TodoService } from './todo.service';
+import { Todo } from './interfaces/todo.interface';
 
 @Controller('todos')
-export class TodoController {}
+export class TodoController {
+  constructor(private readonly todoService: TodoService) {}
+
+  @Get('/')
+  async getTodos(): Promise<Todo[]> {
+    return await this.todoService.getTodos();
+  }
+
+  @Post('/')
+  async addTodo(@Body() todo: Todo): Promise<Todo> {
+    return await this.todoService.addTodo(todo);
+  }
+
+  @Put('/:index')
+  async updateTodo(
+    @Param('index') index: number,
+    @Body() todo: Todo,
+  ): Promise<void> {
+    await this.todoService.updateTodo(index, todo);
+  }
+
+  @Delete('/:index')
+  async deleteTodo(@Param('index') index: number): Promise<void> {
+    await this.todoService.deleteTodo(index);
+  }
+}
