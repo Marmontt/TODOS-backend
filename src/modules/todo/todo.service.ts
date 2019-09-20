02d815d59cd94f99fec49a58abee8afccec87d06
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-20 11:18:38 +0300

    added todo service

--- src/modules/todo/todo.service.ts
+++ src/modules/todo/todo.service.ts
@@ -4,4 +4,21 @@ import { Todo } from './interfaces/todo.interface';
 @Injectable()
 export class TodoService {
   private todos: Todo[] = [];
+
+  async addTodo(todo: Todo): Promise<Todo> {
+    await this.todos.push(todo);
+    return todo;
+  }
+
+  async getTodos(): Promise<Todo[]> {
+    return this.todos;
+  }
+
+  async deleteTodo(index: number): Promise<void> {
+    await this.todos.splice(index, 1);
+  }
+
+  async updateTodo(index: number, todo: Todo): Promise<void> {
+    this.todos[index] = todo;
+  }
 }
