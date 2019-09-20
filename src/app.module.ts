a34f1f98c91a8fe2480256d3361b1b76a25db702
calcutinE <42960376+calcutinE@users.noreply.github.com>
2019-09-20 11:18:06 +0300

    removed shit

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,10 +1,6 @@
 import { Module } from '@nestjs/common';
-import { AppController } from './app.controller';
-import { AppService } from './app.service';
-
+import { TodoModule } from './modules/todo';
 @Module({
-  imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  imports: [TodoModule],
 })
 export class AppModule {}
